74992c65135b0fd2cfae0b43d577a0445dc41934
Franca Ellerman <franca@castedo.com>
2022-04-17 16:35:32 -0400

    Change to setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-from setuptools import setup
-setup()
+from setuptools import find_packages, setup
+
+setup(name='naviance_admissions_calculator',
+      version='0.0.1',
+      install_requires = [
+          'numpy',
+          'pandas',
+          'bs4',
+          'statsmodels']
+      )
